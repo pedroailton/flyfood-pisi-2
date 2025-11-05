2944cc9c473b4158923a6628847dd0a6aec349ca
Pedro Ailton <pedroailton630@gmail.com>
2025-11-05 10:31:40 -0300

    Update parser.py

--- 1VA/flyfood/parser.py
+++ 1VA/flyfood/parser.py
@@ -5,6 +5,8 @@ def lerArquivo(caminho_arquivo):
     Lê o conteúdo de um arquivo de texto (.txt) ou CSV (.csv) e
     retorna uma lista de linhas que representam a matriz do mapa.
     """
+
+    """
     linhas = []
 
     # detecta a extensão do arquivo
@@ -26,6 +28,31 @@ def lerArquivo(caminho_arquivo):
                 linhas.append(linha.strip())
                 
     return linhas # retorno da lista de linhas
+    """
+
+    # Algoritmo de Cícero
+    with open(caminho_arquivo, "r", encoding="utf-8") as objArq:
+        #se você quiser uma lista onde cada objeto será uma string
+        #grande representando uma linha do arquivo:
+        #listaLinhas = objArq.readlines() #obs: cada linha terah um enter junto com o ultimo elemento
+
+        distancias = {}
+
+        for i in range(1, 58): #linhas 1 a 57 pois a 58 nao terá aresta
+            linha = objArq.readline() #le só uma linha do arquivo
+            #transformando a linha em lista de strings:
+            lista = linha.split() #obs: lista de strings (não int)
+
+            for j in range(i+1, 59): #colunas i+1 a 58
+                if len(lista) > 0:
+                    peso = int(lista.pop(0)) #obs: peso int, poderia ser float em outro problema
+                else:
+                    print(f"Erro! linha {i} do arquivo não possui elementos suficientes")
+                    exit()
+                #gravando a aresta em (i, j) e (j, i):
+                distancias[(i,j)] = peso
+                distancias[(j,i)] = peso
+    return distancias
 
 def parseArquivo(caminho_arquivo):
     """
